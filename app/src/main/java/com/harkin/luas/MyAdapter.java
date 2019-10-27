b4a93300ca68c3d44c0e263eedd6b43a8cf9248c
Henry Larkin <hfglarkin@gmail.com>
2019-10-27 19:19:52 +0000

    Migrate to Android X

--- app/src/main/java/com/harkin/luas/MyAdapter.java
+++ app/src/main/java/com/harkin/luas/MyAdapter.java
@@ -1,7 +1,7 @@
 package com.harkin.luas;
 
 import android.app.Activity;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.ViewGroup;
 
 import com.hannesdorfmann.adapterdelegates.AdapterDelegatesManager;
